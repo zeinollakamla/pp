24171c8c50b2f45e26afe81e13dd988f5d9475df
Kamila Zeinolla <z.kamila205@gmail.com>
2019-02-07 15:24:45 +0600

    first

---
+++ lab1/Task2/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student a = new Student("Kamila", "ID120");
+            a.GetInfo();
+        }
+    }
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year;
+
+        public Student(string n, string i)
+        {
+            name = n;
+            id = i;
+            year = 1;
+        }
+        
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Increment the year of study: {++year}");
+        }
+    }
+}
